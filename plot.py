d2a9608728b86d6b62c48681d9a4476e3fe20cfd
Alexandre Beaulne <alexandre.beaulne@gmail.com>
2018-05-07 22:43:10 -0400

    added infinity datapoint to plot

--- plot.py
+++ plot.py
@@ -1,55 +1,42 @@
-import glob
+
 import matplotlib.pyplot as plt
-import numpy as np
-import os
-import csv
-import math
+import pandas as pd
 
+def plot_graph():
 
+    data = pd.read_csv('results.csv')
 
-def plot_graph(save=False):
-    data = np.loadtxt('results.csv', delimiter=',', skiprows=1, dtype = 'str')
+    xticks = ['1', '10', '20', '40', '$\infty$']
 
-    algos = data[:, 0]
-    games = data[:, 1]
-    rollout = np.array(data[:, 2], dtype ='float')
-    avg_score = np.array(data[:, 3], dtype ='float')
+    a3c_pong = data[(data['algo'] == 'A3C') & (data['game'] == 'pong')]
+    nsql_pong = data[(data['algo'] == 'nstepQ') & (data['game'] == 'pong')]
+    a3c_seaquest = data[(data['algo'] == 'A3C') & (data['game'] == 'seaquest')]
+    nsql_seaquest = data[(data['algo'] == 'nstepQ') & (data['game'] == 'seaquest')]
 
-    games_un = np.unique(games)
-    algos_un = np.unique(algos)
-    rollout_un = np.unique(rollout)
-    
-    
-    # n = len(rollout_un)
-    num_games = len(games_un)
-    xarray = rollout[:len(rollout_un)]
-    index = 0
     plt.figure()
-    for i in range(len(games_un)):
-        plt.subplot(1, num_games, i + 1)
-        game = games_un[i]
-        print(game)
-        for j in range(len(algos_un)):
-            alg = algos_un[j]
-            print(alg)
-            scores = []
-            for k in range(len(rollout_un)):
-                scores.append(avg_score[index])
-                index += 1
-            yarray = scores
-            plt.plot(xarray, yarray, label=alg)
-            plt.xlabel('rollout length')
-            plt.ylabel('avg score')
-            print(scores)
-        plt.legend(loc='best')
-        plt.title('{}'.format(game))
+    plt.subplot(1, 2, 1)
+    plt.plot(range(5), a3c_pong['avg_score'], label='A3C')
+    plt.plot(range(5), nsql_pong['avg_score'], label='n-step Q-learning')
+    plt.xticks(range(5), xticks)
+    plt.title('Pong')
+    plt.xlabel('rollout')
+    plt.ylabel('score')
+    plt.legend()
+
+    plt.subplot(1, 2, 2)
+    plt.plot(range(5), a3c_seaquest['avg_score'], label='A3C')
+    plt.plot(range(5), nsql_seaquest['avg_score'], label='n-step Q-learning')
+    plt.xticks(range(5), xticks)
+    plt.xlabel('rollout')
+    plt.ylabel('score')
+    plt.title('Seaquest')
+    plt.legend()
+
     plt.tight_layout()
-    if save:
-        plt.savefig('results.png')
+    plt.savefig('results.png')
     plt.show()
     plt.close()
 
-
-
 if __name__ == '__main__':
-    plot_graph(True)
+    plot_graph()
+
